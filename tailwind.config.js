cc0cba5ef98cd7fec9c536af2b7734db0d4e5c4d
Thomas Misikoff <tmisikoff@gmail.com>
2021-12-27 13:59:34 -0600

    feat: add project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+const configObj = {
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+  },
+}
+
+module.exports = configObj
---
+++ vetur.config.js
@@ -0,0 +1,14 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+// https://vuejs.github.io/vetur/guide/setup.html#advanced
+module.exports = {
+  // support monorepos
+  projects: [
+    {
+      // **required**
+      // Where is your project?
+      // It is relative to `vetur.config.js`.
+      root: '.',
+    },
+  ],
+}
